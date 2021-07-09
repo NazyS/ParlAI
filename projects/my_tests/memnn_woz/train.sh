4f227e53f66539bfb8f9301a1bb63fc0a703049b
NazyS <50199774+NazyS@users.noreply.github.com>
2021-07-09 17:32:02 +0300

    babi dialogue and full woz states as candaidates

--- projects/my_tests/memnn_woz/train.sh
+++ projects/my_tests/memnn_woz/train.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-parlai train_model --candidates fixed --eval-candidates fixed  --fixed-candidates-path labels.dict --eps 1000 --dynamic-batching full  -lr 0.01 --validation-every-n-epochs 1 --save-after-valid True --validation-patience 100 --ignore-bad-candidates True -m memnn -hops 4 --embedding-size 128 -t woz:memnn_woz -mf $1 
+parlai train_model  --candidates inline --eval-candidates inline  --eps 1000  -lr 0.01 --validation-every-n-epochs 1 --save-after-valid True --validation-patience 100 --ignore-bad-candidates True --batchsize 5 -m memnn -hops 3 --embedding-size 128 -t $1 -mf $2 
+
+# --dynamic-batching full
+# --candidates fixed --eval-candidates fixed  --fixed-candidates-path labels_full.dict
