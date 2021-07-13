70fae0c809fab8c4b436a5c19a2c10defb0b50e2
NazyS <nsyakovenko@gmail.com>
2021-07-14 00:49:13 +0300

    dialog_babi task5 test updates

--- projects/my_tests/memnn_woz/train.sh
+++ projects/my_tests/memnn_woz/train.sh
@@ -1,6 +1,47 @@
 #!/bin/bash
 
-parlai train_model  --candidates inline --eval-candidates inline  --eps 1000  -lr 0.01 --validation-every-n-epochs 1 --save-after-valid True --validation-patience 100 --ignore-bad-candidates True --batchsize 5 -m memnn -hops 3 --embedding-size 128 -t $1 -mf $2 
-
-# --dynamic-batching full
-# --candidates fixed --eval-candidates fixed  --fixed-candidates-path labels_full.dict
+# use multiprocessing_train for utilizing multiple gpus
+parlai train_model \
+                --candidates fixed \
+                --eval-candidates fixed \
+                --interactive-candidates fixed \
+                --fixed-candidates-path task5/candidates.txt \
+                --eps 1000 \
+                -lr 0.05 \
+                --optimizer adam \
+                --validation-every-n-epochs 10 \
+                --save-after-valid True \
+                --validation-patience 100 \
+                --ignore-bad-candidates True \
+                --batchsize 100 \
+                -m memnn \
+                -hops 3 \
+                --embedding-size 32 \
+                --task dialog_babi:task:5 \
+                -mf task5/tf_pos_enc/memnn_dialog_babi \
+                --history-size 10 \
+                --time-features True \
+                --position-encoding True \
+                --tensorboard-log True
+                # --num-workers 4 \
+                # --train-predict True \
+                        # default is False
+                # --dynamic-batching full \
+                        # truncate is needed 
+                # --fp16 True \
+                # --fp16-impl mem_efficient \                        
+                # --optimizer mem_eff_adam \                
+                # --repeat-blocking-heuristic True \
+                        # default is True
+                # --memsize 32 \
+                        # default is 32
+                # --dict-file task5/dialog_babi_task5.dict \
+                # -mf $1 \
+                        # model file to save/init
+                # --rank-top-k -1 \
+                        # default is -1
+                # --inference max \
+                        # topk or max
+                # --topk 5 
+                # --return-cand-scores False \
+                        # default is False                
